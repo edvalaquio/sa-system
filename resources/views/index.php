61b2a3138993c4803a90bc7594017f1c10f5d1c4
LasingNaJedi69 <nuj101nuj@gmail.com>
2018-04-16 10:59:13 +0800

    Adding changes from March 2018

--- resources/views/index.php
+++ resources/views/index.php
@@ -20,6 +20,8 @@
 	<script type="text/javascript" src="/bower_components/angular-route/angular-route.js"></script>
 	<script type="text/javascript" src="/bower_components/bootstrap/dist/js/bootstrap.js"></script>
 	<script type="text/javascript" src="/bower_components/angular-bootstrap/ui-bootstrap.js"></script>
+	<script type="text/javascript" src="/bower_components/angular-bootstrap/ui-bootstrap.js"></script>
+	<script type="text/javascript" src="/bower_components/angular-bootstrap/ui-bootstrap-tpls.js"></script>
 	<script type="text/javascript" src="/js/index.js"></script>
 	<script type="text/javascript" src="/js/controllers/userController.js"></script>
 	<script type="text/javascript" src="/js/controllers/filesLogController.js"></script>
