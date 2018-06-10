e7982df57a8fd1a0c3161c2b2ab088120202e02f
beltjun26 <bar@example.com>
2018-06-11 02:35:31 +0800

    Transaction UI created.:

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -1,24 +1,37 @@
 @extends('layouts.app')
 
 @push('styles')
+    <link href="{{ asset('css/home.css') }}" rel="stylesheet">
     <link href="{{ asset('css/transaction.css') }}" rel="stylesheet">
 @endpush
 
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col s12 m6 offset-m3 l4 offset-l4">
-            <div class="card card-default">
-                <div class="card-body">
-                    <a href="#" class="title"><h4 class="truncate">Requires Signature from Dean</h4></a>
-                    <p class="particular">Requires Signature from Dean</p>
-                    <section class="additional-details">
-                        <span class="type received"><i class="material-icons">inbox</i>&nbsp;Received,&nbsp;</span>
-                        <span class="timestamp">March 2, 2018, 9:32 am</span>
-                        <span class="status pending">Pending</span>
-                    </section>
-                </div>
-            </div>
+        <div class="col s12">
+            <a href="#" class="title"><h4 class="truncate">Requires Signature from Dean</h4></a>
+            <section class="additional-details">
+                <span class="type received"><i class="material-icons">inbox</i>&nbsp;Received,&nbsp;</span>
+                <span class="timestamp">March 2, 2018, 9:32 am</span>
+                <span class="status pending">Pending</span>
+            </section>
+            <ul>
+                <li class="chip">DTR</li>
+                <li class="chip">Thesis</li>
+            </ul>
+            <p class="particular">Requires Signature from Dean. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+
+            <h6>Attachments:</h6>
+            <ul class="files">
+                <li><i class="material-icons amber-text">attach_file</i> Guidelines_for_Thesis.pdf</li>
+            </ul>
+
+            <p class="note">
+                This is a note. Users can add these to inform the receiver of the document.
+            </p>
+
+            <h6>History</h6>
+            <p class="note">To be worked on.</p>
         </div>
     </div>
 </div>
